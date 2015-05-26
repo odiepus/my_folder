0917ecee1d176d122ac8b3541dc1ee083afe04fd
hector <odiepus@gmail.com>
2015-05-26 16:29:04 -0700

    completed Vars ex3, 4
    completed Vars ex3, 4

--- name.rb
+++ name.rb
@@ -1,4 +1,25 @@
+#name.rb by Hector Herrera
+#May 26 2015
+#variables exersice 1, 3, 4
+
+puts "Exercise 1\n"
 puts "Please input your name: "
 name = gets.chomp
 
 puts "Hello, #{name}."
+puts "\n"
+
+puts "Exercise 3\n"
+10.times do
+  puts name + "\n"
+end
+puts "\n"
+
+puts "Exercise 4\n"
+puts "Please input your first name: "
+first_Name = gets.chomp
+
+puts "Please input your second name: "
+last_Name = gets.chomp
+
+puts "Hello, #{first_Name} #{last_Name}."
