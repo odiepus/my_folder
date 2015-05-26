508aec3bbea8cd88ddc318b11c2886b9c3bcad77
hector <odiepus@gmail.com>
2015-05-26 14:53:00 -0700

    exercise 2 Vars
    completed ex.2 vars section

---
+++ age.rb
@@ -0,0 +1,8 @@
+puts "Hello, how old are you?\n"
+age = gets.chomp
+age = age.to_i
+
+years = Array.new
+years = [10, 20, 30, 40]
+
+years.each {|item|  puts "In #{item} years you will be #{(item + age)}"}
