e965a993950ec95fb0b458fe2af4322a0ea3fbb4
hector <odiepus@gmail.com>
2015-05-26 17:49:55 -0700

    completed Vars ex.6

--- varsEx5_6.rb
+++ varsEx5_6.rb
@@ -1,6 +1,6 @@
 #name.rb by Hector Herrera
 #May 26 2015
-#Variable exercise 5
+#Variable exercise 5, 6
 
 puts %Q(Variable Exercise 5
       Case 1:
@@ -9,3 +9,9 @@ puts %Q(Case 2:
       x is in an inner block. Thus, we do not have access to it from the outer block.
       We would then get an error if we tried to run the code.
       y is accessible from outer block and is equal to 3.)
+
+puts "\n"
+#Exercise 6
+puts %Q(Variable Exercise 6
+      'shoes' is called on line 3 in irb console. Either 'shoes'
+      was never initialized or it is in another block of code.)
