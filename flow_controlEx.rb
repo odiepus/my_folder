4fb5d550869cb61b302322a4ac71a4f994848398
hector <odiepus@gmail.com>
2015-05-26 21:16:05 -0700

    flow control
    c/w ex. 4, 5, 6 fixed 3 to notify user of a value less than 0

--- flow_controlEx.rb
+++ flow_controlEx.rb
@@ -35,5 +35,57 @@ when (0..50)
 when (51..100)
   puts "Integer is between 50 and 100"
 else
-  puts "Integer is above 100"
+  if input_Num < 0
+    puts "Did not input a positive integer"
+  else
+    puts "Integer is above 100"
+  end
 end
+puts ""
+
+#Exercise 4
+puts "Exercise 4 Flow Control
+1. FALSE
+2. Did you get it right?
+3. Alright now!\n\n"
+
+#Exercise 5
+puts "Exercise 5 Flow Control
+Already did it in case form.\n"
+
+def check_num(input_Num)
+  case input_Num
+
+  when (0..50)
+    puts "Integer is between 0 and 50"
+  when (51..100)
+    puts "Integer is between 50 and 100"
+  else
+    if input_Num < 0
+      puts "Did not input a positive integer"
+    else
+      puts "Integer is above 100"
+    end
+  end
+end
+
+puts "Please input a positive integer: "
+input_User_Num = gets.chomp.to_i
+
+check_num(input_User_Num)
+puts ""
+
+#Exercise 6
+puts "Exercise 6 Flow Control
+We get this error because the method was not ended with an 'end'. The 'end'
+that is there belongs to the if statment."
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
